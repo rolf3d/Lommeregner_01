c01ba182e0efba9feebdd38f4b4fc8d1b0d9d200
Rolf Petersen <rollep7@msn.com>
2016-11-01 10:46:02 +0100

    Add project files.

---
+++ Lommeregner_01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lommeregner_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double input01 = 0.0;
+            double input1 = 0.0;
+            double input2 = 0.0;
+            string valg = "";
+            double resultat = 0.0;
+
+            double input01 = Convert.ToDouble(Console.ReadLine());
+            double input02 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(input01);
+
+            if (valg == "+")
+            {
+                resultat = input1 + input2;
+            }
+            else if (valg == "-")
+            {
+                resultat = input1 - input2;
+            }
+            else if (valg == "*")
+            {
+                resultat = input1 * input2;
+            }
+            else
+            {
+                Console.WriteLine("Du skal vælge + - eller * tegn! ");
+            }
+
+
+      
+        }
+    }
+}
---
+++ Lommeregner_01/regner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lommeregner_01
+{
+    class regner
+    {
+        double input1 = 0.0;
+        double input2 = 0.0;
+        string valg;
+
+        public regner()
+        {
+
+        }
+
+        
+        
+    }
+}
